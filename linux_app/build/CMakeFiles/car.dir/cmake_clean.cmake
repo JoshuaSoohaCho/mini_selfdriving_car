e55d27695c66c166f8f6f0f499e6effcefa61680
JoshuaCho2025 <jsc9820@nyu.edu>
2026-05-12 18:17:36 -0400

    CMake updates

---
+++ linux_app/build/CMakeFiles/car.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/car.dir/main.c.obj"
+  "CMakeFiles/car.dir/main.c.obj.d"
+  "car.exe"
+  "car.exe.manifest"
+  "car.pdb"
+  "libcar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/car.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
